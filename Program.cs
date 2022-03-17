c95999829f18026584ccd5fce5006ff5ac0f2964
PallaviKatari <pallavikatari@gmail.com>
2022-03-17 14:50:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace UnitTest
+{
+    public class Program
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public static void Main()
+        {
+            Program program=new Program();
+           int result= program.Add(10,20);
+            Console.WriteLine(result);
+        }
+    }
+}
